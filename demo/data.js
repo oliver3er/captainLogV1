bd3298cb3239f8ae67b767ed69132bdb3fcbb41d
deanchen <antlet2004@gmail.com>
2017-12-21 11:07:37 +0800

    [add] d3 mindmap demo

---
+++ demo/data.js
@@ -0,0 +1,52 @@
+const data = [
+	{
+		_id : '1',
+		name : 'root',
+	},
+	{
+		_id : '2',
+		name : 'logger',
+		parentId : '1',
+	},
+	{
+		_id : '3',
+		name : 'v1.0',
+		parentId : '2',
+	},
+	{
+		_id : '3.1',
+		name : 'v1.1',
+		parentId : '2',
+	},
+	{
+		_id : '3.2',
+		name : 'v1.2',
+		parentId : '2',
+	},
+	{
+		_id : '4',
+		name : 'todo',
+		parentId : '1',
+	},
+	{
+		_id : '5',
+		name : '日记',
+		parentId : '1',
+	},
+	{
+		_id : '6',
+		name : 'test',
+		parentId : '1',
+	},
+	{
+		_id : '8',
+		name : 'day',
+		parentId : '1',
+	},
+	{
+		_id : '9',
+		name : 'javascript',
+		parentId : '1',
+	},
+
+]
